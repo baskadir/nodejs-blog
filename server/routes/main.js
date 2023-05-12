980e838239df8c1e1dfec0c13db601837ac14077
baskadir <kadirbas09@gmail.com>
2023-05-12 20:02:41 +0300

    Get list of posts in home page

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,13 +1,38 @@
 const express = require("express");
 const router = express.Router();
+const Post = require("../models/Post");
 
 // Routes
-router.get("", (req, res) => {
-  const locals = {
-    title: "NodeJs Blog",
-    description: "Simple blog created with NodeJs, Express & MongoDb",
-  };
-  res.render("index", { locals });
+
+// GET- Home
+router.get("", async (req, res) => {
+  try {
+    const locals = {
+      title: "NodeJs Blog",
+      description: "Simple blog created with NodeJs, Express & MongoDb",
+    };
+
+    let perPage = 6;
+    let page = req.query.page || 1;
+
+    const data = await Post.aggregate([{ $sort: { createdAt: -1 } }])
+      .skip(perPage * page - perPage)
+      .limit(perPage)
+      .exec();
+
+    const count = await Post.count();
+    const nextPage = parseInt(page) + 1;
+    const hasNextPage = nextPage <= Math.ceil(count / perPage);
+
+    res.render("index", {
+      locals,
+      data,
+      current: page,
+      nextPage: hasNextPage ? nextPage : null,
+    });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 router.get("/about", (req, res) => {
